80ad99893e9b62e770c8c076e3d06bf05908ddac
dan-4s <43755880+dan-4s@users.noreply.github.com>
2020-04-24 21:22:22 -0400

    Update FFE.m

--- FFE.m
+++ FFE.m
@@ -1,7 +1,12 @@
 function [ output ] = FFE( response )
 %FFE calculates the FFE response of an input channel response
+%the normalized coefficients were calculated in another piece of code that is not
+%presented here. See Q3_4706_Final.m
 normalized_FFE_coefs = [ 0.591311905168975; -0.145323987006326;  0.172465378697213; -0.0908987291274862];
 
+%this code just iterates through the input response calculating the 
+%output values at each time step. This can also be done with a matrix. 
+%See Q2_4706_final.m for examples of this. 
 input = zeros(1,length(response) + 6);
 input(4:length(input)-3) = response;
 output = zeros(1, length(response));

418893bf56a30223eec84af41a6ae9f6388de6ef
dan-4s <43755880+dan-4s@users.noreply.github.com>
2020-04-24 21:20:15 -0400

    Update DFE.m

--- DFE.m
+++ DFE.m
@@ -1,11 +1,16 @@
 function [ DFE_output ] = DFE( response )
-
+%Example code for how to solve a DFE. This code was run for question 3
+%where the post cursors needed to be canceled. Hence all the coeficients are
+%calculated based on the known main-cursor value.
 main_cursor = response(4);
+
+%calculate the DFE coefficients. 
 DFE_coefs = zeros(9, 1);
 for i = 1: length(DFE_coefs)
     DFE_coefs(i) = -response(4+i)/main_cursor;
 end
 
+%calculate the DFE output given the input response. 
 DFE_output = zeros(1, length(response)+9);
 DFE_input = response;
 for i = 10:length(DFE_output)
